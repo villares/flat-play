d6050e082e570b93f26d7a3413c7556b5980d6ae
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2020-08-29 19:39:51 -0300

    ugh!

--- polygons_try.py
+++ polygons_try.py
@@ -1,4 +1,3 @@
-from flask import Flask
 import random
 from bezmerizing import Polyline
 from flat import document, shape, rgb, rgba
@@ -10,31 +9,29 @@ def t_normal(a, b, mu, sigma):
     tn = truncnorm((a-mu)/sigma, (b-mu)/sigma, loc=mu, scale=sigma)
     return tn.rvs(1)[0]
 
-app = Flask(__name__)
-
-@app.route('/')
 def draw():
     size = 200
     d = document(size, size, 'mm')
     page = d.addpage()
-    
-    uniform_fig = shape().nostroke().stroke(rgba(255, 0, 0, 240)).width(1).nofill()
-    normal_fig = shape().nostroke().stroke(rgba(64, 220, 64, 240)).width(1.5).nofill()
-    t_normal_fig = shape().nostroke().stroke(rgba(0, 128, 255, 240)).width(2).nofill()
-    mu = 50 # center of normal distributions
-    sigma = 20 # standard deviation of normal distributions
-    sample_n = 400 # number of "samples" (i.e. circles to draw for each distribution)
-    for i in range(sample_n):
-        c = uniform_fig.circle(uniform(100), uniform(100), 1)
-        page.place(c)
-    for i in range(sample_n):
-        c = normal_fig.circle(normal(mu, sigma), normal(mu, sigma), 1.5)
-        page.place(c)
-    for i in range(sample_n):
-        c = t_normal_fig.circle(t_normal(20, 80, mu, sigma), t_normal(20, 80, mu, sigma), 2)
-        page.place(c)
-        
-    return page.svg()
+    fig = shape()
+    page.place(fig.polygon( ((10, 10), (100, 10), (10, 100)) ) )   
+    # uniform_fig = shape().nostroke().stroke(rgb(255, 0, 0)).width(1).nofill()
+    # normal_fig = shape().nostroke().stroke(rgb(64, 220, 64)).width(1.5).nofill()
+    # t_normal_fig = shape().nostroke().stroke(rgb(0, 128, 255)).width(2).nofill()
+    # mu = 50 # center of normal distributions
+    # sigma = 20 # standard deviation of normal distributions
+    # sample_n = 400 # number of "samples" (i.e. circles to draw for each distribution)
+    # for i in range(sample_n):
+    #     c = uniform_fig.circle(uniform(100), uniform(100), 1)
+    #     page.place(c)
+    # for i in range(sample_n):
+    #     c = normal_fig.circle(normal(mu, sigma), normal(mu, sigma), 1.5)
+    #     page.place(c)
+    # for i in range(sample_n):
+    #     c = t_normal_fig.circle(t_normal(20, 80, mu, sigma), t_normal(20, 80, mu, sigma), 2)
+    #     page.place(c)
+    # d.pdf("test.pdf")
+    return page.svg("test.svg")
 
-if __name__ == '__main__':
-    app.run()
+
+draw()
